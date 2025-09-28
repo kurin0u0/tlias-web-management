22c2d61cc1799572ddc5edf431766a96e6783e16
kurin0u0 <kurin0u0@gmail.com>
2025-09-28 16:33:32 +1000

    28_09

--- src/main/java/com/saki/controller/DeptController.java
+++ src/main/java/com/saki/controller/DeptController.java
@@ -4,7 +4,9 @@ import com.saki.pojo.Dept;
 import com.saki.pojo.Result;
 import com.saki.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +16,8 @@ public class DeptController {
 
     @Autowired
     private DeptService deptService;
-    @RequestMapping("/depts")
+    // @RequestMapping (value = "/depts",method = RequestMethod.GET) // method: point out request ways
+    @GetMapping("depts")
     public Result list(){
         System.out.println("查询全部的部门数据");
         List<Dept> deptList=deptService.findall();
