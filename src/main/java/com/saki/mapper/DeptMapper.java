1c36d5cb21ca1915156a9e6c832d14fe1925f276
kurin0u0 <kurin0u0@gmail.com>
2025-09-29 22:44:15 +1000

    29_09

--- src/main/java/com/saki/mapper/DeptMapper.java
+++ src/main/java/com/saki/mapper/DeptMapper.java
@@ -2,6 +2,7 @@ package com.saki.mapper;
 
 import com.saki.pojo.Dept;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -9,7 +10,15 @@ import java.util.List;
 @Mapper
 public interface DeptMapper {
 
+    // 结果封装
+    /* @Result({
+            @Result(column = "create_time", property = "createTime"),
+            @Result(column = "update_time", property = "updateTime"),
+    })
+     */
+
     // search all data
-    @Select("select * from dept order by update_time desc;")
+    @Select("select id,name,create_time, update_time from dept order by update_time desc;")
+    // 起别名 create_time createTime, update_time updateTime
     List<Dept> findall();
 }
